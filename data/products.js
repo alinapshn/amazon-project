d07aa93ba80e68b24e10ca33eb0c4316c63429eb
alinapshn <alinapshenichnaya06@gmail.com>
2025-09-10 16:59:10 +0300

    added "add to cart" message

--- data/products.js
+++ data/products.js
@@ -656,5 +656,25 @@ const products = [
       "apparel",
       "mens"
     ]
-  }
+  },
+    {
+    id: "id1",
+    image: "../images/products/backpack.jpg",
+    name: "Backpack",
+    rating: {
+      stars: 4,
+      count: 76,
+    },
+    priceCents: 400,
+  },
+  {
+    id: "id2",
+    image: "../images/products/umbrella.jpg",
+    name: "Umbrella",
+    rating: {
+      stars: 4.5,
+      count: 146,
+    },
+    priceCents: 2899,
+  },
 ];
