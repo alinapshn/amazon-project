a7925b419ecb3cb35664c9d50b67f458e7f86db7
alinapshn <alinapshenichnaya06@gmail.com>
2025-09-12 12:04:53 +0300

    make cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,4 +1,4 @@
-import { cart, addToCart } from "../data/cart.js";
+import { cart, addToCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
@@ -66,20 +66,17 @@ document.querySelector(".js-products-grid").innerHTML = productsHTML;
 const addedMessageTimeouts = {};
 
 function updateCartQuantity(productId) {
-  let cartQuantity = 0;
-
-  cart.forEach((cartItem) => {
-    cartQuantity += cartItem.quantity;
-  });
-
+  const cartQuantity = calculateCartQuantity();
   document.querySelector(".js-cart-quantity").innerHTML = cartQuantity;
 
+  if (!productId) return; 
+
   const addedMessage = document.querySelector(`.js-added-to-cart-${productId}`);
+  if (!addedMessage) return;
 
   addedMessage.classList.add("added-to-cart-visible");
 
   const previousTimeoutId = addedMessageTimeouts[productId];
-
   if (previousTimeoutId) {
     clearTimeout(previousTimeoutId);
   }
@@ -90,6 +87,9 @@ function updateCartQuantity(productId) {
 
   addedMessageTimeouts[productId] = timeoutId;
 }
+
+
+updateCartQuantity();
 
 document.querySelectorAll(".js-add-to-cart").forEach((button) => {
   button.addEventListener("click", () => {
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,9 +1,10 @@
-import { cart } from "../data/cart.js";
+import { cart, removeFromCart, calculateCartQuantity } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
-import { removeFromCart } from "../data/cart.js";
 
 let cartSummaryHTML = "";
+
+updateCartQuantity();
 
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
@@ -103,5 +104,15 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
       `.js-cart-item-container-${productId}`
     );
     container.remove();
+
+    updateCartQuantity();
   });
 });
+
+function updateCartQuantity() {
+  const cartQuantity = calculateCartQuantity();
+
+  document.querySelector(
+    ".js-return-to-home-link"
+  ).innerHTML = `${cartQuantity} items`;
+}
