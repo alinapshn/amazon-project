7aea4535b2ea3b4f2bf7029784302532ccbcc6dd
alinapshn <alinapshenichnaya06@gmail.com>
2025-09-19 14:06:05 +0300

    create Applience class

--- data/products.js
+++ data/products.js
@@ -58,6 +58,29 @@ class Clothing extends Product {
   }
 }
 
+class Appliance extends Product {
+  instructionsLink;
+  warrantyLink;
+
+  constructor(productDetails) {
+    super(productDetails);
+    this.instructionsLink = productDetails.instructionsLink;
+    this.warrantyLink = productDetails.warrantyLink;
+  }
+
+  extraInfoHTML() {
+    // super.extraInfoHTML();
+    return `
+    <a href = "${this.instructionsLink}" target = "_blank">
+      Instructions
+    </a>
+     <a href = "${this.warrantyLink}" target = "_blank">
+      Warranty
+    </a>
+    `;
+  }
+}
+
 // const date = new Date();
 // console.log(date);
 // console.log(date.toLocaleTimeString());
@@ -130,6 +153,9 @@ export const products = [
     },
     priceCents: 1899,
     keywords: ["toaster", "kitchen", "appliances"],
+    type: "applience",
+    instructionsLink: "/images/appliance-instructions.png",
+    warrantyLink: "/images/appliance-warranty.png",
   },
   {
     id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
@@ -264,6 +290,9 @@ export const products = [
     },
     priceCents: 3074,
     keywords: ["water boiler", "appliances", "kitchen"],
+    type: "applience",
+    instructionsLink: "/images/appliance-instructions.png",
+    warrantyLink: "/images/appliance-warranty.png",
   },
   {
     id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
@@ -477,6 +506,9 @@ export const products = [
     },
     priceCents: 2250,
     keywords: ["coffeemakers", "kitchen", "appliances"],
+    type: "applience",
+    instructionsLink: "/images/appliance-instructions.png",
+    warrantyLink: "/images/appliance-warranty.png",
   },
   {
     id: "02e3a47e-dd68-467e-9f71-8bf6f723fdae",
@@ -521,6 +553,9 @@ export const products = [
     },
     priceCents: 10747,
     keywords: ["food blenders", "kitchen", "appliances"],
+    type: "applience",
+    instructionsLink: "/images/appliance-instructions.png",
+    warrantyLink: "/images/appliance-warranty.png",
   },
   {
     id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
@@ -578,6 +613,8 @@ export const products = [
 ].map((productDetails) => {
   if (productDetails.type === "clothing") {
     return new Clothing(productDetails);
+  } else if (productDetails.type === "applience") {
+    return new Appliance(productDetails);
   }
   return new Product(productDetails);
 });
