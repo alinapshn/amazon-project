9ec7a3175def4e4b92b6a3e376c8b41e364a6174
alinapshn <alinapshenichnaya06@gmail.com>
2025-09-20 12:35:40 +0300

    learn error handling

--- data/products.js
+++ data/products.js
@@ -124,6 +124,9 @@ export function loadProductsFetch() {
         return new Product(productDetails);
       });
       console.log("load products");
+    })
+    .catch((error) => {
+      console.log("unexpected error. please try again later");
     });
 
   return promise;
@@ -148,6 +151,10 @@ export function loadProducts(fun) {
     console.log("load products");
 
     fun();
+  });
+
+  xhr.addEventListener("error", (error) => {
+    console.log("unexpected error. please try again later");
   });
 
   xhr.open("GET", "https://supersimplebackend.dev/products");
