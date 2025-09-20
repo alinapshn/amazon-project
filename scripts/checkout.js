0ec8105c292ed2d21cd40b869e00745da369f8cf
alinapshn <alinapshenichnaya06@gmail.com>
2025-09-20 13:42:09 +0300

    delete car.js

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,6 @@
 import { renderOrderSummary } from "./checkout/orderSummary.js";
 import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { renderCheckoutHeader } from "./checkout/checkoutHeader.js";
-import "../data/car.js";
 import { loadProducts, loadProductsFetch } from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 // import "../data/cart-class.js";
